768b2aecb73b07b6bf079b12c959b860a358181f
hadysoufan <hadisoufan74@gmail.com>
2022-09-11 19:19:30 +0300

    adding the required files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './components/authentication/authentication.component';
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>I am the shop page</h1>
-    </div>
-  );
-};
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.components';
 
 const App = () => {
   return (
@@ -18,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
