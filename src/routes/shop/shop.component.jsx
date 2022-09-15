53899f3b3820ca9ef628f21b7334c74203d26292
hadysoufan <hadisoufan74@gmail.com>
2022-09-15 21:11:40 +0300

    updating categories context into categories reducer

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { getCategoriesAndDocument } from '../../utils/firebase/firebase.util';
+import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import {setCategories} from "../../store/categories/category.action"
 import './shop.styles.scss';
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocument('categories');
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategoriesMap();
+  }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
