481098a3143a1fb99b0ccf0ab24a12ab082a4546
hadysoufan <hadisoufan74@gmail.com>
2022-09-15 21:05:13 +0300

    updating user context to use user reducer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import Home from './routes/home/home.component';
 import Authentication from './components/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.components';
+import { useEffect } from 'react';
+import { setCurrentUser } from './store/user/user.action';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.util';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener(user => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
