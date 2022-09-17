896311ff4144bb88a9d273cbcb7c246ea910ed8e
hadysoufan <hadisoufan74@gmail.com>
2022-09-17 09:47:45 +0300

    adding redux-persist

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { store, persistor } from './store/store';
+import reportWebVitals from './reportWebVitals';
 
 import './index.scss';
 
-const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -20,6 +21,7 @@ render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
+
+reportWebVitals();
