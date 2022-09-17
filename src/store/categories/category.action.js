cf2b099a189101401d338018e36844edaf455344
hadysoufan <hadisoufan74@gmail.com>
2022-09-17 09:50:51 +0300

    updating categories thunk to saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,5 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTION_TYPE } from './category.types';
-import { getCategoriesAndDocument } from '../../utils/firebase/firebase.util';
 
 export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
@@ -11,17 +10,6 @@ export const fetchCategoriesSuccess = categoriesArray =>
     categoriesArray
   );
 
-export const fetchCategoriesFailure = error =>
+export const fetchCategoriesFailed = error =>
   createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
 
-export const fetchCategoriesAsync = () => {
-  return async dispatch => {
-    dispatch(fetchCategoriesStart());
-    try {
-      const categoriesArray = await getCategoriesAndDocument('categories');
-      dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch (error) {
-      dispatch(fetchCategoriesFailure(error));
-    }
-  };
-};
